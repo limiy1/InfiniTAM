de8cbe1beae16c1b34a911e1965255b694c82448
unknown <LMYang06@gmail.com>
2016-04-15 14:59:41 +0900

    Compile version on windows 8.1

---
+++ InfiniTAM/script/teddy.bat
@@ -0,0 +1,1 @@
+C:/workspace/InfiniTAM/InfiniTAM/build/Debug/InfiniTAM.exe C:/workspace/InfiniTAM/InfiniTAM/Files/Teddy/calib.txt C:/workspace/InfiniTAM/InfiniTAM/Files/Teddy/Frames/%04i.ppm C:/workspace/InfiniTAM/InfiniTAM/Files/Teddy/Frames/%04i.pgm
